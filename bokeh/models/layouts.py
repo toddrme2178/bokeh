6dca019bbfb49feaf934ce837859b4ece4795566
Mateusz Paprocki <mattpap@gmail.com>
2018-09-18 00:11:40 +0200

    Allow setting simple track sizing

--- bokeh/models/layouts.py
+++ bokeh/models/layouts.py
@@ -99,16 +99,16 @@ class Spacer(LayoutDOM):
 
     '''
 
+QuickTrackSizing = Either(Enum("auto", "min", "max"), Int)
+
 RowSizing = Either(
-    Enum("auto", "min", "max"),
-    Int,
+    QuickTrackSizing,
     Struct(policy=Enum("auto", "min", "max"), align=Enum(TrackAlign)),
     Struct(policy=Enum("fixed"), height=Int, align=Enum(TrackAlign)),
     Struct(policy=Enum("flex"), factor=Float, align=Enum(TrackAlign)))
 
 ColSizing = Either(
-    Enum("auto", "min", "max"),
-    Int,
+    QuickTrackSizing,
     Struct(policy=Enum("auto", "min", "max"), align=Enum(TrackAlign)),
     Struct(policy=Enum("fixed"), width=Int, align=Enum(TrackAlign)),
     Struct(policy=Enum("flex"), factor=Float, align=Enum(TrackAlign)))
@@ -120,10 +120,10 @@ class GridBox(LayoutDOM):
     children = List(Tuple(Instance(LayoutDOM), Int, Int), default=[], help="""
     """)
 
-    rows = Dict(IntOrString, RowSizing, default={}, help="""
+    rows = Either(QuickTrackSizing, Dict(IntOrString, RowSizing), help="""
     """)
 
-    cols = Dict(IntOrString, ColSizing, default={}, help="""
+    cols = Either(QuickTrackSizing, Dict(IntOrString, ColSizing), help="""
     """)
 
 @abstract
@@ -170,7 +170,7 @@ class Row(Box):
     that is a sequence, or using the ``children`` keyword argument.
     '''
 
-    cols = Dict(IntOrString, ColSizing, default={}, help="""
+    cols = Either(QuickTrackSizing, Dict(IntOrString, ColSizing), help="""
     """)
 
 class Column(Box):
@@ -180,8 +180,9 @@ class Column(Box):
     that is a sequence, or using the ``children`` keyword argument.
     '''
 
-    rows = Dict(IntOrString, RowSizing, default={}, help="""
+    rows = Either(QuickTrackSizing, Dict(IntOrString, RowSizing), help="""
     """)
+
 
 class Panel(Model):
     ''' A single-widget container with title bar and controls.
